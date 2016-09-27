afe6dcc61819ed796469e2178b7787ff97511176
Teddy Ma <shijie.ma@gmail.com>
2016-09-27 22:38:54 +0800

    update push script

--- push.ps1
+++ push.ps1
@@ -1,7 +1,7 @@
-.nuget\nuget push CoreProfiler\bin\Release\*.symbols.nupkg
+.nuget\nuget push CoreProfiler\bin\Release\*.symbols.nupkg -Source https://nuget.smbsrc.net/
 del CoreProfiler\bin\Release\*.symbols.nupkg
-.nuget\nuget push CoreProfiler\bin\Release\*.nupkg
+.nuget\nuget push CoreProfiler\bin\Release\*.nupkg -Source https://www.nuget.org
 
-.nuget\nuget push CoreProfiler.Web\bin\Release\*.symbols.nupkg
+.nuget\nuget push CoreProfiler.Web\bin\Release\*.symbols.nupkg -Source https://nuget.smbsrc.net/
 del CoreProfiler.Web\bin\Release\*.symbols.nupkg
-.nuget\nuget push CoreProfiler.Web\bin\Release\*.nupkg
+.nuget\nuget push CoreProfiler.Web\bin\Release\*.nupkg -Source https://www.nuget.org
